3661243c4d063a6f1674df95b51e616e52074267
wesley <151071658+wesley-08@users.noreply.github.com>
2024-08-09 07:39:40 -0300

    Add files via upload

---
+++ User.js
@@ -0,0 +1,20 @@
+//classe
+class User {
+    constructor(fullname, email, password) {
+      this.fullname = fullname;
+      this.email = email;
+      this.password = password;
+    }
+  
+    login (email, password) {
+      if (this.email === email && this.password === password) {
+        console.log(`${this.fullname}: Login success!`);
+      } else {
+        console.log(`${this.fullname}: Login failed!`);
+      }
+    }
+  }
+  
+  const user = new User('John Doe', 'john@email.com', 'securePassword');
+  user.login('john@email.com', 'securePassword');
+  user.login('john@email.com', 'wrongPassword'); 
