6bf14024b49772293122c89b6877f9a4171a1712
wesley <151071658+wesley-08@users.noreply.github.com>
2024-08-14 08:08:50 -0300

    ajustes

--- Product.js
+++ Product.js
@@ -1,31 +1,31 @@
 class Product {
     constructor(name, description, price) {
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.inStock = 0;
+        this.name = name
+        this.description = description
+        this.price = price
+        this.inStock = 0
     }
-
+  
     addToStock(quantity) {
-        this.inStock += quantity;
+        this.inStock += quantity
         console.log(`
-            Nome do produto: ${product.name}
-            Descrição do produto: ${product.description}
-            Valor do produto: R$${product.price.toFixed(2)}
-            Quantidade no estoque: ${product.inStock}
-            `);
+            Nome do produto: ${this.name}
+            Descrição do produto: ${this.description}
+            Valor do produto: R$${this.price.toFixed(2)}
+            Quantidade do produto: ${this.inStock}
+        `)
     }
-
+  
     calculateDiscount(discount) {
         this.price = this.price - (discount * this.price / 100)
         console.log(`
-                ==================================
-                Desconto no produto: ${discount}%
-                Novo valor do produto: R$${newPrice.toFixed(2)}
-            `)
+            ===========================
+            Desconto no produto: ${discount}%
+            Novo valor do produto: ${this.price.toFixed(2)}
+        `)
     }
-}
-
-const Notebook = new Product("Notebook", "Notebook Nitro 5", 1000.00);
-Notebook.addToStock(10)
-Notebook.calculateDiscount(5)
+  }
+  
+  const notebook = new Product('Notebook', 'Notebook Nitro 5', 1000)
+  notebook.addToStock(10)
+  notebook.calculateDiscount(5)
